ad72ea064338f47963629b583db8d53adef17848
Aaron Rodriguez <aaroner@yahoo.com>
2023-06-14 13:28:44 -0500

    express server & initial product routes

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -7,7 +7,6 @@ import products from '../products';
 const ProductScreen = () => {
   const { id: productId } = useParams();
   const product = products.find((p) => p._id === productId);
-  console.log(product);
 
   return (
     <>
