d7eb98c0a2d2228dcb510e0d45d40cd380a0d80f
Aaron Rodriguez <aaroner@yahoo.com>
2023-06-21 00:09:24 -0500

    get products from database

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 // need file extension for local imported files
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 
 connectDB(); // connect to MongoDB
@@ -15,16 +15,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-// route for all products
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-// route for a single product
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 // start server?
 app.listen(port, () => console.log(`Server running on port ${port}`));
