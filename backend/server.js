37e579d8560ad5b56cac264e258381e06fbb9f2e
Aaron Rodriguez <aaroner@yahoo.com>
2023-06-21 12:32:17 -0500

    custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 // need file extension for local imported files
 import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+// error handlers
+app.use(notFound);
+app.use(errorHandler);
 
 // start server?
 app.listen(port, () => console.log(`Server running on port ${port}`));
