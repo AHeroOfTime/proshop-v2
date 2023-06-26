b2a3ee054df54dc04abfff455ccdd6494ddb73c6
Aaron Rodriguez <aaroner@yahoo.com>
2023-06-26 12:34:49 -0500

    products api slice & get products endpoint

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,16 @@
+import { PRODUCTS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    // product queries
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productsApiSlice;
