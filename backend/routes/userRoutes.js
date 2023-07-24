bac5d9ce14c4d4d5f83f906bda00d6fe2cb6cf02
Aaron Rodriguez <aaroner@yahoo.com>
2023-07-24 15:54:01 -0500

    auth middleware & endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,16 +11,24 @@ import {
   getUserByID,
   updateUser,
 } from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 // post req => register, get => admin, get all users
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 // route to logout
 router.post('/logout', logoutUser);
 // to login
-router.post('/login', authUser);
+router.post('/auth', authUser);
 // route to get profile or update profile
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 // routes for users
-router.route('/:id').delete(deleteUser).get(getUserByID).put(updateUser);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser);
 
 export default router;
