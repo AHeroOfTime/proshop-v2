55838d6060af400afaec1f9ca9907526a2564d2f
Aaron Rodriguez <aaroner@yahoo.com>
2023-07-24 14:22:17 -0500

    user email & password auth

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ connectDB(); // connect to MongoDB
 // initialize server/express/app
 const app = express();
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
