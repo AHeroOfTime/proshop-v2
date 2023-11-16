ba4ce9e1e6b68be3619e483b8143d2719ad8638b
Aaron Rodriguez <aaroner@yahoo.com>
2023-11-15 20:21:45 -0600

    creating products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 // route for all products
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 // route for single product
 router.route('/:id').get(getProductById);
 
