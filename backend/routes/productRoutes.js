4d59f68b4e5284e4ce69c5635cd3bfebe82d12ff
Aaron Rodriguez <aaroner@yahoo.com>
2023-11-20 21:49:09 -0600

    create reviews backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -17,5 +18,7 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+// route for adding a review
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
