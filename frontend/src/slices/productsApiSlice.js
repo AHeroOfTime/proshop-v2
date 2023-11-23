4ff88ab9bd35de808b24c9938a2cd7299a4204b4
Aaron Rodriguez <aaroner@yahoo.com>
2023-11-22 18:59:26 -0600

    paginate products

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -5,8 +5,11 @@ export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     // product queries
     getProducts: builder.query({
-      query: () => ({
+      query: ({ pageNumber }) => ({
         url: PRODUCTS_URL,
+        params: {
+          pageNumber,
+        },
       }),
       keepUnusedDataFor: 5,
       providesTags: ['Products'],
