4b925d7e9919b82ef74a53608442487a19829c67
Aaron Rodriguez <aaroner@yahoo.com>
2023-11-24 20:59:20 -0600

    page titles

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -25,7 +25,6 @@ const ProductEditScreen = () => {
   const {
     data: product,
     isLoading,
-    refetch,
     error,
   } = useGetProductDetailsQuery(productId);
 
@@ -91,7 +90,6 @@ const ProductEditScreen = () => {
       <FormContainer>
         <h1>Edit Product</h1>
         {loadingUpdate && <Loader />}
-        {loadingUpload && <Loader />}
 
         {isLoading ? (
           <Loader />
@@ -133,6 +131,7 @@ const ProductEditScreen = () => {
                 onChange={uploadFileHandler}
               ></Form.Control>
             </Form.Group>
+            {loadingUpload && <Loader />}
 
             <Form.Group controlId='brand' className='my-2'>
               <Form.Label>Brand</Form.Label>
